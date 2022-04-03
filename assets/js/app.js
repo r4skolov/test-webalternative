8f9c335d82081cf5f763d3a6458466976018c117
Artem Davydchenkov <davydchenkov1@gmail.com>
2022-04-03 21:09:14 +0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -202,7 +202,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-var CLASS_ACTIVE = 'active';
+var CLASS_ACTIVE = 'visible';
 var wrapList = document.querySelectorAll('.js-tabs');
 
 var tabs = function () {
